bd1a29ba7d9d684e0f44a693a6015d2ee9588498
harshilgoti2209 <harshilgoti2209@gmail.com>
2020-09-15 14:12:21 +0530

    Saving to git

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import Nav from "./Nav";
 import Home from "./Home";
 import Service from "./Service";
